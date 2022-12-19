ba717ba9e5c5927a0ebaeb8e96416f1b6cb7584c
root <graybismuthmaster@hotmail.com>
2022-12-19 09:08:29 -0500

    Prueba de despliegue en EC2

--- lib/features/call/screens/call_screen.dart
+++ lib/features/call/screens/call_screen.dart
@@ -1,4 +1,4 @@
-import 'package:agora_uikit/agora_uikit.dart';
+// import 'package:agora_uikit/agora_uikit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:espe_chat/common/widgets/loader.dart';
@@ -23,53 +23,53 @@ class CallScreen extends ConsumerStatefulWidget {
 }
 
 class _CallScreenState extends ConsumerState<CallScreen> {
-  AgoraClient? client;
+  // AgoraClient? client;
   String baseUrl = 'https://whatsapp-clone-rrr.herokuapp.com';
 
   @override
   void initState() {
     super.initState();
-    client = AgoraClient(
-      agoraConnectionData: AgoraConnectionData(
-        appId: AgoraConfig.appId,
-        channelName: widget.channelId,
-        tokenUrl: baseUrl,
-      ),
-    );
+    // client = AgoraClient(
+    //   agoraConnectionData: AgoraConnectionData(
+    //     appId: AgoraConfig.appId,
+    //     channelName: widget.channelId,
+    //     tokenUrl: baseUrl,
+    //   ),
+    // );
     initAgora();
   }
 
   void initAgora() async {
-    await client!.initialize();
+    // await client!.initialize();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: client == null
-          ? const Loader()
-          : SafeArea(
-              child: Stack(
-                children: [
-                  AgoraVideoViewer(client: client!),
-                  AgoraVideoButtons(
-                    client: client!,
-                    disconnectButtonChild: IconButton(
-                      onPressed: () async {
-                        await client!.engine.leaveChannel();
-                        ref.read(callControllerProvider).endCall(
-                              widget.call.callerId,
-                              widget.call.receiverId,
-                              context,
-                            );
-                        Navigator.pop(context);
-                      },
-                      icon: const Icon(Icons.call_end),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+      // body: client == null
+      //     ? const Loader()
+      //     : SafeArea(
+      //         child: Stack(
+      //           children: [
+                //   AgoraVideoViewer(client: client!),
+                //   AgoraVideoButtons(
+                //     client: client!,
+                //     disconnectButtonChild: IconButton(
+                //       onPressed: () async {
+                //         await client!.engine.leaveChannel();
+                //         ref.read(callControllerProvider).endCall(
+                //               widget.call.callerId,
+                //               widget.call.receiverId,
+                //               context,
+                //             );
+                //         Navigator.pop(context);
+                //       },
+                //       icon: const Icon(Icons.call_end),
+                //     ),
+                //   ),
+    //             ],
+    //           ),
+    //         ),
     );
   }
 }
